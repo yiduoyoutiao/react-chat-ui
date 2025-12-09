3442db06cd2de2f6639e21c56fa9d80c8f3470a4
gaojunyu <gao.junyu@asia-quest.jp>
2025-12-09 19:21:13 +0900

    first commit

---
+++ my-app/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+// 导入 ChatUI 组件
+import ChatUI from './features/chatui/ChatUI';
+
+function App() {
+  return (
+      <ChatUI />
+  );
+}
+
+export default App;
